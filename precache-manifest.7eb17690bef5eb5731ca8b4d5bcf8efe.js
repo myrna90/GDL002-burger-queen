df3c9b7393204e233b67391b8a249aa9a3eb7d88
unknown <myrnamares90@gmail.com>
2019-05-28 12:01:03 -0500

    Updates

---
+++ precache-manifest.7eb17690bef5eb5731ca8b4d5bcf8efe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15b0d808afb55e8129a38e68f7a91a6",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "2602dbe7be3f9945c37d",
+    "url": "/GDL002-burger-queen/static/css/main.84facf53.chunk.css"
+  },
+  {
+    "revision": "d9ac5302396f32aa8aff",
+    "url": "/GDL002-burger-queen/static/js/2.b643c63b.chunk.js"
+  },
+  {
+    "revision": "2602dbe7be3f9945c37d",
+    "url": "/GDL002-burger-queen/static/js/main.b67a992a.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  }
+]);
